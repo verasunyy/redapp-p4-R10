c0382ca91f836dd0d3ca832343f45353b0f8b6e0
verasunyy <verasun@live.com>
2019-08-26 02:46:13 -0700

    add map

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -9,10 +9,27 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-
+import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
 const Map = ({ allConducts }) => {
   return (
-    <Text>Map</Text>
+    <MapView
+      style={{ flex: 1 }}
+      region={{
+        latitude: 49.263489,
+        longitude: -123.131348,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121
+      }}
+    >
+      <MapView.Marker
+        coordinate={{
+          latitude: 49.263489,
+          longitude: -123.131348
+        }}
+        image={require('../../assets/images/map_pin.png')}
+      />
+    </MapView>
   )
 }
 
--- js/screens/Map/MapContainer.js
+++ js/screens/Map/MapContainer.js
@@ -10,13 +10,19 @@ import {
   StatusBar,
 } from 'react-native';
 
-
+import { typography } from '../../config/styles'
 import Map from './Map'
 
 
 class MapContainer extends Component {
   static navigationOptions = {
     title: 'Map',
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: '500',
+      fontSize: 22,
+      fontFamily: typography.fontMain,
+    },
   };
   render() {
 
